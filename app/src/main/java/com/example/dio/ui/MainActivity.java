e884e5bfd44b9782e6cfe65c772bf6f7602dfb3f
JRRL <jrrlino157@gmail.com>
2022-07-10 21:54:40 -0300

    Firebase, Crashanalytics

--- app/src/main/java/com/example/dio/ui/MainActivity.java
+++ app/src/main/java/com/example/dio/ui/MainActivity.java
@@ -17,6 +17,8 @@ import com.example.dio.domain.Match;
 import com.example.dio.ui.adapter.MatchesAdapter;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -29,7 +31,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MatchesAPI matchesApi;
-    private MatchesAdapter matchesAdapter;
+    private MatchesAdapter matchesAdapter = new MatchesAdapter(Collections.emptyList());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -58,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+            //throw new RuntimeException("Teste de Crashlytics");
         });
     }
 
@@ -68,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     private void setupMatchesList() {
         binding.rvMatches.setHasFixedSize(true);
         binding.rvMatches.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvMatches.setAdapter(matchesAdapter);
         findMatchesFromAPI();
     }
 
